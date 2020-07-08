4901a90c55953321c4ed0ae24c237ad13ec072a5
Gabriel Henrique <gabriel.holiveira@live.com>
2020-07-08 11:17:41 -0300

    main

--- src/main/initQueueProcess.ts
+++
@@ -1,6 +0,0 @@
-import { initQueueProcess } from "../useCaseBuilder/initQueueProcess"
-import { Queue } from "../core/domain/queue"
-
-export default function (queue: Queue, process: () => Promise<void>): void {
-    initQueueProcess(queue, process) 
-}
--- src/useCaseBuilder/initQueueProcess.ts
+++
@@ -1,7 +0,0 @@
-import { InitQueueProcess } from "../core/usecase/initQueueProcess";
-import { Queue } from "../core/domain/queue";
-
-export function initQueueProcess(queue: Queue, process: () => Promise<void>): void {
-    const initQueueProcess: InitQueueProcess = new InitQueueProcess(queue, process)
-    initQueueProcess.init()
-}
